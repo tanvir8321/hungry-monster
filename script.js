dcc7b6477bdef8ea0ccb77bff751cab7957f1ff2
tansovir <tanvir8321@gmail.com>
2021-04-16 12:07:21 +0600

    search button work

---
+++ script.js
@@ -0,0 +1,111 @@
+// search funtion call api and display desire searched data.
+const search = value => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${value}`)
+        .then(res => res.json())
+        .then(data => {
+            if (data.meals) {
+                const mealContainer = document.getElementById('meal-container');
+                mealContainer.innerHTML = '';
+                data.meals.forEach(food => {
+                    const mealDiv = document.createElement('div');
+                    const mealContent = `
+                        <a href="">
+                            <div class="meal">
+                                <img src="${food.strMealThumb}" alt="">
+                                <p>${food.strMeal}</p>
+                            </div>
+                        </a>
+                    `
+                    mealDiv.innerHTML = mealContent;
+                    mealContainer.appendChild(mealDiv);
+                    
+                });
+            } else {
+                const mealContainer = document.getElementById('meal-container');
+                const mealContent = `
+                    <a href="">
+                        <div class="meal">
+                            <p>Not found....</p>
+                        </div>
+                    </a>`
+                mealContainer.innerHTML = mealContent;
+            }
+        });
+    }
+
+    // get value from user by input
+    const getInputValue = () => {
+        return document.getElementById('search-input').value;
+    }
+
+    // on input handler :: when user some input value in search are, then onInputHandler will work that time
+    const onInputHandler = () => {
+        const onInputSearch = getInputValue();
+        search(onInputSearch);
+    }
+    // onClickHandler :: when user click on search button, then onClickHandler with work
+    const onClickHandler = () => {
+        const onInputSearch = getInputValue();
+        search(onInputSearch);
+    }
+
+// const search = () => {
+
+//     const searchValue = document.getElementById("search-input").value;
+
+//     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`)
+//         .then(res => res.json())
+//         .then(data => displayFood(data.meals));
+
+
+//     const mealContainer = document.getElementById("meal-container");
+//     const displayFood = foods => {
+//         if (foods) {
+//             foods.forEach(food => {
+//                 const mealValue =
+//                     `<a href="">
+//                        <div class="meal">
+//                            <img src="${food.strMealThumb}" alt="">
+//                            <p>${food.strMeal}</p>
+//                        </div>
+//                    </a>`;
+//                 mealContainer.innerHTML = mealValue;
+//             });
+//         } else {
+//             const mealContainer = document.getElementById("meal-container");
+//             const li = document.createElement('li');
+//             li.innerText = 'Sorry not found your meal...';
+//             mealContainer.appendChild(li);
+//         }
+//     }
+// }
+
+
+// const searchValue = '';
+
+// fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`)
+//     .then(res => res.json())
+//     .then(data => displayFood(data.meals));
+
+
+// const mealContainer = document.getElementById("meal-container");
+// let mealValue = '';
+// const displayFood = foods => {
+//     if (foods) {
+//         foods.forEach(food => {
+//             mealValue +=
+//                 `<a href="">
+//                    <div class="meal">
+//                        <img src="${food.strMealThumb}" alt="">
+//                        <p>${food.strMeal}</p>
+//                    </div>
+//                </a>`;
+//             mealContainer.innerHTML = mealValue;
+//         });
+//     } else {
+//         const mealContainer = document.getElementById("meal-container");
+//         const li = document.createElement('li');
+//         li.innerText = 'Sorry not found your meal...';
+//         mealContainer.appendChild(li);
+//     }
+// }
